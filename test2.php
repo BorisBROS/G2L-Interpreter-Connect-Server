fd00a451e9a5b98505c767c90141ea8ff677e1e6
nathanathan <nbnate@uw.edu>
2012-01-23 15:33:31 -0800

    updating with contents of test1.php

--- test2.php
+++ test2.php
@@ -1,3 +1,36 @@
 <?php
-error_log("user: ".$_REQUEST['user']);
+
+//require_once('voxeo-sms-lib.php');
+require_once('mysql-db-lib.php');
+
+function echo_options()
+{
+        $conn = get_db_connection();
+
+        $query = "SELECT language_name_string FROM languages";
+        $result = mysql_query($query) or die(mysql_error());
+
+        $i = 1;
+
+        while ($row = mysql_fetch_assoc($result)) {
+                echo("Press " . $i . " for " . $row["language_name_string"] . ".\n");
+                $i++;
+        }
+}
+
+echo('<?xml version="1.0" encoding="UTF-8" ?>');
 ?>
+
+<callxml version="3.0">
+        <prompt choices="1,2">
+                <?php echo_options(); ?>
+        </prompt>
+        <on event="choice:1">
+                <say>one</say>
+        </on>
+        
+        <on event="choice:2">
+                <say>two</say>
+        </on>
+        <wait value="10s"/> 
+</callxml>
