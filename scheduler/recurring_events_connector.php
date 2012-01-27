1700739ed1f4a888db3a91b4bea1a2e3314f9e26
nathanathan <nbnate@uw.edu>
2012-01-27 13:13:13 -0800

    -

--- scheduler/recurring_events_connector.php
+++ scheduler/recurring_events_connector.php
@@ -1,10 +1,10 @@
 <?php
 	require ('config.php');
 	//include ('codebase/scheduler_connector.php');
-	//include ('codebase/db_sqlsrv.php');
-	require ('mobile_scheduler/codebase/connector/scheduler_connector.php');
-	require ('mobile_scheduler/codebase/connector/db_sqlsrv.php');
 	
+	require ('mobile_scheduler/codebase/connector/scheduler_connector.php');
+	//require ('mobile_scheduler/codebase/connector/db_sqlsrv.php');
+	//require ('codebase/db_sqlsrv.php');
 	
 	error_log("start of script");
 
