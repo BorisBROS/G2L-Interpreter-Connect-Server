82bd281ca76794502ed318b5d2cfed59fe50a9fb
nathanathan <nbnate@uw.edu>
2012-01-27 17:55:46 -0800

    -

--- scheduler/mobile_scheduler.php
+++ scheduler/mobile_scheduler.php
@@ -1,13 +1,9 @@
 <!DOCTYPE html> 
 <html lang="en"> 
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1"> 
-	<title>Single page template</title> 
-	<!-- <link rel="stylesheet" href="//code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.css" />
-	<script src="http://code.jquery.com/jquery-1.6.4.min.js"></script> -->
-
-	
+<head> 
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" /> 
+	<title>jQueryMobile - DateBox Demos</title>
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.css" />
 	<link type="text/css" href="http://dev.jtsage.com/cdn/datebox/latest/jquery.mobile.datebox.min.css" rel="stylesheet" /> 
 	<link type="text/css" href="http://dev.jtsage.com/jQM-DateBox/css/demos.css" rel="stylesheet" /> 
 	
@@ -19,38 +15,79 @@
 	<script type="text/javascript" src="http://dev.jtsage.com/cdn/datebox/latest/jquery.mobile.datebox.min.js"></script>
 	<script type="text/javascript" src="http://dev.jtsage.com/gpretty/prettify.js"></script>
 	<link type="text/css" href="http://dev.jtsage.com/gpretty/prettify.css" rel="stylesheet" />
-	
-	<script type="text/javascript" charset="utf-8">
+	<script type="text/javascript">
 		$('div').live('pagecreate', function() {
 			prettyPrint()
 		});
 	</script>
-</head> 
-
-<body> 
-
-<div data-role="page">
-
-	<div data-role="header">
-		<h1>Single page</h1>
-	</div><!-- /header -->
-
-	<div data-role="content">	
+	
+</head>
+<body>
+<div data-role="page" data-theme="c" id="droIndex"> 
+	<div data-role="header"> 
+		<h1>jQueryMobile - DateBox</h1>
+		<a href="../" data-icon="home" data-iconpos="notext">Home</a>
+	</div>
+	
 	<label for="mydate">Some Time</label>
 
 	<input name="mydate" id="mydate" type="date" data-role="datebox"
 	   data-options='{"mode": "timebox", "timeFormatOverride": 12}'>
-	   
-		<p>This is a single page boilerplate template that you can copy to build you first jQuery Mobile page. Each link or form from here will pull a new page in via Ajax to support the animated page transitions.</p>		
-		<p>Just view the source and copy the code to get started. All the CSS and JS is linked to the jQuery CDN versions so this is super easy to set up. Remember to include a meta viewport tag in the head to set the zoom level.</p>
-		<p>This template is standard HTML document with a single "page" container inside, unlike a <a href="multipage-template.html" data-ajax="false">multi-page template</a> that has multiple pages within it. We strongly recommend building your site or app as a series of separate pages like this because it's cleaner, more lightweight and works better without JavaScript.</p>
-	</div><!-- /content -->
 	
-	<div data-role="footer">
-		<h4>Footer content</h4>
-	</div><!-- /footer -->
+	<div data-role="content" data-theme="c">
+		<div class="content-secondary"> 
 	
-</div><!-- /page -->
+			<div id="jqm-homeheader"> 
+				<h1 id="jqm-logo"><img src="../jquery-logo-db.png" alt="jQuery Mobile Framework :: DateBox" /></h1> 
+				<p>A Date and Time Picker plugin for jQueryMobile</p> 
+				
+			</div> 
+			
+			<ul data-role="listview" data-inset="true" data-theme="c" data-dividertheme="f"> 
+				<li data-role="list-divider">Android Demos</li> 
+				<li data-theme="a">Simple Options</li> 
+				<li><a href="limit.html">Range Limiting &amp; Picking</a></li>
+			</ul> 
+			
+		</div><!--/content-primary-->	
+		
+		<div class="content-primary"> 
+			<nav> 
+				<h2>Android Default</h2>
+				
+				<div data-role="fieldcontain">
+					<label for="defandroid">Some Date</label><input name="defandroid" type="date" data-role="datebox" id="defandroid" />
+				</div>
+				
+				<p>Using the android mode is as simple as setting the 'mode' option to "datebox".</p>
+				
+				<pre class="prettyprint">&lt;label for="mydate"&gt;Some Date&lt;/label&gt;
 
-</body>
+&lt;input name="mydate" id="mydate" type="date" data-role="datebox"
+   data-options='{"mode": "datebox"}'&gt;</pre>
+				
+				
+				<h2>Credit Cards</h2>
+				
+				<div data-role="fieldcontain">
+					<label for="droidcard">Expires</label><input name="droidcard" type="date" data-role="datebox" data-options='{"dateFormat": "mm/YYYY", "fieldsOrderOverride": ["m", "y"]}' id="droidcard" />
+				</div>
+				
+				<p>Credit Card expiration dates can be gathered by setting 'dateFormat' to "mm/YYYY" and 'fieldsOrder' to ["m", "y"]</p>
+				
+				<pre class="prettyprint">&lt;label for="mydate"&gt;Some Date&lt;/label&gt;
+
+&lt;input name="mydate" id="mydate" type="date" data-role="datebox"
+   data-options='{"mode": "datebox", "dateFormat": "mm/YYYY", "fieldsOrderOverride": ["m", "y"]}'}'&gt;</pre>
+				
+			</nav> 
+		</div> 
+		
+	</div>
+	<div data-role="footer">
+		<div data-role="controlgroup" data-type="horizontal">
+			<a data-role="button" href="https://github.com/jtsage/jquery-mobile-datebox">GitHub Source</a><a data-role="button" rel='external' href="http://dev.jtsage.com/blog/">Blog</a><a data-role="button" href="mailto:jtsage+datebox@gmail.com">Contact</a><a data-role="button" href="http://jquerymobile.com/">jQueryMobile Homepage</a>
+		</div>
+	</div>
+</div>
 </html>
