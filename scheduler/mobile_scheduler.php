d12a465d266991dc38fa732ff768b5459e1e50f0
nathanathan <nbnate@uw.edu>
2012-01-30 11:12:41 -0800

    -

--- scheduler/mobile_scheduler.php
+++ scheduler/mobile_scheduler.php
@@ -27,8 +27,7 @@
 <div data-role="page" class="type-interior">
 
     <div data-role="header"> 
-		<h1>jQueryMobile - DateBox</h1>
-		<a href="../" data-icon="home" data-iconpos="notext">Home</a>
+		<h1>Interpreter Availability Scheduler</h1>
 	</div>
 
 	<div data-role="content">
@@ -37,25 +36,21 @@
 require_once('config.php');
 
 function int_to_day($int){
-	$days = array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+	$days = array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
 	return $days[$int];
 }
+/**
+ * This is used with the regular expression for parsing rec_type to produce a list of days.
+ */
 function get_days($matches)
 {
 	// as usual: $matches[0] is the complete match
 	// $matches[1] the match for the first subpattern
 	// enclosed in '(...)' and so on
-	
 	array_shift($matches);
 	$matches = array_filter($matches);
 	$matches = array_map("int_to_day", $matches);
-	
-	/*
-	foreach ($matches as $match){
-		$return_value .= $days[$match]. ', ';
-	}
-	*/
-	return implode(',', $matches);
+	return implode(', ', $matches);
 }
 
 try {
@@ -72,17 +67,25 @@ try {
 		$event_id = $row['event_id'];
 		$start_date = new DateTime($row['start_date']);
 		$event_start = $start_date->format('h:i A');
-		$event_length = new DateInterval('PT'.$row['event_length'].'S');
-		$end_date = $start_date->add($event_length);
-		$event_end = $end_date->format('h:i A');
 		
-		$event_days = preg_replace_callback(
-		            "|.*_.?_.?_.?_(0?),?(1?),?(2?),?(3?),?(4?),?(5?),?(6?)#.*|",
-		            "get_days",
-		            $row['rec_type']);
+		$event_length = new DateInterval('PT'.$row['event_length'].'S');
+		
+		$end_date = new DateTime($row['start_date']);
+		$end_date->add($event_length);
+		
+		$event_end = $end_date->format('h:i A');
+		if($row['rec_type']){
+			$event_days = preg_replace_callback(
+			            "|.*_.?_.?_.?_(0?),?(1?),?(2?),?(3?),?(4?),?(5?),?(6?)#.*|",
+			            "get_days",
+			            $row['rec_type']);
+		}
+		else{
+			$event_days = $start_date->format('m/d/Y');
+		}
 		 
 ?>
-		<li><a href="mobile_scheduler_edit.php?<?php echo $event_id ?>">
+		<li><a href="mobile_scheduler_edit.php?<?php echo $event_id ?>" data-rel="dialog" data-transition="pop">
 			
 				<h3><?php echo("$event_start - $event_end"); ?></h3>
 				<p><strong><?php echo($event_days); ?></strong></p>
@@ -103,9 +106,14 @@ catch(PDOException $e) {
 		</ul>
 
 	</div><!-- /content -->
+	
+	<div data-role="footer">
+    		<fieldset class="ui-grid-a">
+				<div class="ui-block-a"><button type="submit" data-theme="d">Add Time</button></div>
+    	    </fieldset>
+	</div><!-- /footer -->
 
-
-	</div><!-- /page -->
+</div><!-- /page -->
 
 </body>
 </html>
