142b2e92271c3e1fc75e256fee951d01071e4d15
nathanathan <nbnate@uw.edu>
2012-01-30 16:56:20 -0800

    -

--- scheduler/g2l_shared_code.php
+++ scheduler/g2l_shared_code.php
@@ -4,6 +4,7 @@ $days = array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 
  * This is used with the regular expression for parsing rec_type to produce a list of days.
  */
 function int_to_day($int){
+	global $days;
 	return $days[$int];
 }
 ?>
